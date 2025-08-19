cd1d9e9777cda3fa15ea4c5a66982f80df99f4bf
P-Clemente <pedroclemente2209@gmail.com>
2025-08-18 22:02:11 -0500

    first commit

---
+++ src/test/java/com/proyectopia/pia/PiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectopia.pia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
